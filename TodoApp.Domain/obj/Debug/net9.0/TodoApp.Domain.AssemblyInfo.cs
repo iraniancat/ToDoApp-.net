de9e576a1f9b0885404d0d9df8f977145d97765b
Navid <navid.beyranvand@gmail.com>
2025-05-06 08:55:26 +0330

    reamde.md

--- TodoApp.Domain/obj/Debug/net9.0/TodoApp.Domain.AssemblyInfo.cs
+++ TodoApp.Domain/obj/Debug/net9.0/TodoApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928424272fcd3ff767d49b9ffbcfdecea7754f75")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
